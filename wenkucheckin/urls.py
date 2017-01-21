9d9799000bfcf74337457fab0d5bb2064d5a9662
houshengandt <houshengandt@gmail.com>
2017-01-21 21:29:07 +0800

    完善文库自动签到2: * delete_url 去掉正则后的$。 * 完善文库首页

--- wenkucheckin/urls.py
+++ wenkucheckin/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='wenkuindex'),
     url(r'^add-baidu-account/', views.add_baidu_account, name='add_account'),
-    url(r'^delete-baidu-account/(?P<pk>.*)/$', views.delete_baidu_account, name='delete_account'),
+    url(r'^delete-baidu-account/(?P<pk>.*)/', views.delete_baidu_account, name='delete_account'),
     url(r'^verify-login/', views.verify_login, name='verify_login'),
 ]
