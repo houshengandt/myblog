b5714ad30415cadc099d5db979d266b6f02c55ce
houshengandt <houshengandt@gmail.com>
2017-02-19 18:11:54 +0800

    remove all files

--- blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Article, Comment, Tags
-from markdownx.admin import MarkdownxModelAdmin
-
-
-admin.site.register(Article, MarkdownxModelAdmin)
-admin.site.register(Tags)
--- blog/tests/test_views.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-
-class IndexViewTest(TestCase):
-
-    def test_index_render_the_correct_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'blog/index.html')
